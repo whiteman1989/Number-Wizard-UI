0220fdaa4d721ad3a99954b9898ddccb376ff857
AndreyGorban <Whiteman1989@gmail.com>
2021-03-29 20:22:04 +0300

    Add project files.

---
+++ Assets/Scrypts/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField]
+    private int max = 1000;
+    [SerializeField]
+    private int min = 1;
+    [SerializeField]
+    private Text guessText;
+    private int curentMax;
+    private int curentMin;
+    private int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+
+    public void OnPressHigher()
+    {
+        curentMin = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        curentMax = guess;
+        NextGuess();
+    }
+
+    private int GuessCalc()
+    {
+        return Random.Range(curentMin, curentMax);
+    }
+
+    private void NextGuess()
+    {
+        guess = GuessCalc();
+        guessText.text = guess.ToString();
+        Debug.Log($"New min: {curentMin}, new max {curentMax}.");
+    }
+
+    private void StartGame()
+    {
+        curentMax = max;
+        curentMin = min;
+        NextGuess();
+        curentMax += 1;
+    }
+}
---
+++ Assets/Scrypts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    const int StartSceneIndex = 0;
+
+    public void LoadNextScene()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(StartSceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
